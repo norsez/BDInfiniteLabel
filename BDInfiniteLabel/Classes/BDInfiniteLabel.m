a9d8c4407ac6f9eb016332e62b750a336e103e26
Norsez Orankijanan <norsez@gmail.com>
2015-02-28 11:49:51 +0700

    layout subviews

--- BDInfiniteLabel/Classes/BDInfiniteLabel.m
+++ BDInfiniteLabel/Classes/BDInfiniteLabel.m
@@ -7,7 +7,7 @@
 //
 
 #import "BDInfiniteLabel.h"
-@interface BDInfiniteLabel ()
+@interface BDInfiniteLabel () <UIScrollViewDelegate>
 {
   UIScrollView* _scrollView;
   UILabel *_label;
@@ -22,20 +22,24 @@
 
 - (void)_initialize
 {
+  self.backgroundColor = [UIColor clearColor];
   _scrollView = [[UIScrollView alloc] initWithFrame:CGRectZero];
-  _scrollView.pagingEnabled = YES;
+  _scrollView.delegate = self;
+  _scrollView.backgroundColor = [UIColor clearColor];
   _scrollView.showsVerticalScrollIndicator = NO;
   _scrollView.showsHorizontalScrollIndicator = NO;
-  _scrollView.scrollEnabled = NO;
+//  _scrollView.scrollEnabled = NO;
+  //_scrollView.pagingEnabled = YES;
   _scrollView.bounces = NO;
   _label = [[UILabel alloc] initWithFrame:CGRectZero];
   _label.numberOfLines = 1;
+  _label.lineBreakMode = NSLineBreakByClipping;
   _nextButton = [[UIButton alloc] initWithFrame:CGRectZero];
-  _nextButton.backgroundColor = [UIColor clearColor];
+//  _nextButton.backgroundColor = [UIColor clearColor];
   [_nextButton setTitle:@"▷" forState:UIControlStateNormal];
   [_nextButton addTarget:self action:@selector(didPressNext:) forControlEvents:UIControlEventTouchUpInside];
   _prevButton = [[UIButton alloc] initWithFrame:CGRectZero];
-  _prevButton.backgroundColor = [UIColor clearColor];
+//  _prevButton.backgroundColor = [UIColor clearColor];
   [_prevButton setTitle:@"◁" forState:UIControlStateNormal];
   [_prevButton addTarget:self action:@selector(didPressPrev:) forControlEvents:UIControlEventTouchUpInside];
   [self addSubview:_scrollView];
@@ -48,17 +52,31 @@
   constraints = [constraints arrayByAddingObjectsFromArray: [NSLayoutConstraint constraintsWithVisualFormat:@"|-(0)-[_scrollView]-(0)-|" options:0 metrics:nil views:views]];
   constraints = [constraints arrayByAddingObject:[NSLayoutConstraint constraintWithItem:_prevButton attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeWidth multiplier:0.15 constant:1]];
   constraints = [constraints arrayByAddingObject:[NSLayoutConstraint constraintWithItem:_nextButton attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeWidth multiplier:0.15 constant:1]];
-  constraints = [constraints arrayByAddingObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"|-(0)-[_prevButton] [_nextButton]-(0)-|" options:0 metrics:nil views:views]];
+  constraints = [constraints arrayByAddingObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"|-(0)-[_prevButton]-(>=8)-[_nextButton]-(0)-|" options:0 metrics:nil views:views]];
   constraints = [constraints arrayByAddingObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(0)-[_prevButton]-(0)-|" options:0 metrics:nil views:views]];
   constraints = [constraints arrayByAddingObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(0)-[_nextButton]-(0)-|" options:0 metrics:nil views:views]];
   constraints = [constraints arrayByAddingObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(0)-[_scrollView]-(0)-|" options:0 metrics:nil views:views]];
                  
 }
 
+- (void)layoutSubviews
+{
+  [super layoutSubviews];
+  _scrollView.frame = self.bounds;
+  
+  CGRect boundingRect = [_label.attributedText boundingRectWithSize:CGSizeMake(CGFLOAT_MAX, CGRectGetHeight(self.bounds)) options:NSStringDrawingUsesFontLeading | NSStringDrawingUsesLineFragmentOrigin context:0];
+  _label.frame = boundingRect;
+  _scrollView.contentSize = boundingRect.size;
+  
+}
+#pragma mark - scrollview delegate
+
+
+#pragma mark - properties and methods
 - (void)_updateText
 {
   _label.attributedText = self.attributedString;
-  
+  [self setNeedsLayout];
 }
 
 - (CGSize)intrinsicContentSize
@@ -77,7 +95,7 @@
   
 }
 
-- (void)setAttributedString:(NSString *)attributedText
+- (void)setAttributedString:(NSAttributedString *)attributedText
 {
   if ([_attributedString isEqual:attributedText]) {
     return;
@@ -87,6 +105,7 @@
   [self _updateText];
 }
 
+#pragma mark - init
 - (instancetype)initWithCoder:(NSCoder *)aDecoder
 {
   self = [super initWithCoder:aDecoder];
