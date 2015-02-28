b0a12e1afdbca6fa68ba2687921e540fa94c9889
Norsez Orankijanan <norsez@gmail.com>
2015-02-28 11:32:11 +0700

    add xib for testing

--- BDInfiniteLabel/ViewController.m
+++ BDInfiniteLabel/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import "BDInfiniteLabel.h"
+
+#define kLyrics @"I want to be… I want to be. Someday I want to be. One shining star. Shine on where you are. Oh why. Oh why. My baby, won't you smile? I will not stop trying until your tears are dried."
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet BDInfiniteLabel *infiniteLabel;
 
 @end
 
@@ -16,12 +20,25 @@
 
 - (void)viewDidLoad {
   [super viewDidLoad];
-  // Do any additional setup after loading the view, typically from a nib.
+  self.infiniteLabel.attributedString = [self _longSampleText];
 }
 
-- (void)didReceiveMemoryWarning {
-  [super didReceiveMemoryWarning];
-  // Dispose of any resources that can be recreated.
+
+- (NSAttributedString*)_longSampleText
+{
+  NSMutableAttributedString* astr = [[NSMutableAttributedString alloc] initWithString:kLyrics attributes:@{}];
+  
+  NSArray* fonts = @[[UIFont systemFontOfSize:20], [UIFont systemFontOfSize:10], [UIFont boldSystemFontOfSize:15]];
+  
+  NSInteger lastIndex = 0;
+  for (UIFont *f in fonts) {
+    NSInteger len = MIN(arc4random_uniform(7), kLyrics.length);
+    [astr setAttributes:@{NSFontAttributeName: f} range:(NSRange){lastIndex, len}];
+    lastIndex += len;
+  }
+  
+  return astr;
 }
+
 
 @end
